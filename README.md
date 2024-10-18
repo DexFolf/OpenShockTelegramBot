c86a895fa12ed2452a380db6dcb25a269926661d
Dex Blueberry <github@dexblueberry.xyz>
2024-10-18 05:21:40 +0100

    More customization and error checking added

--- README.md
+++ README.md
@@ -19,10 +19,18 @@ Set environment variables via the .env file or through your terminal.
 
 `API_ID` and `API_HASH` is from your Telegram application (my.telegram.org)
 
-`SHOCK_API` and `SHOCK_ID` is an API token and Shocker ID fetched from [ShockLink](https://shocklink.net/).
+`SHOCK_API` and `SHOCK_ID` is an API token and Shocker ID fetched from [ShockLink](https://openshock.app/).
 To get your Shocker ID, find the shocker you want and click the 3 dots, Edit, then copy the ID.
 
 ## Additional parameters (Optional)
+`SOUND_ALLOWED` `VIBE_ALLOWED` `SHOCK_ALLOWED` controls if the bot permits usage of the respective command. (Default `True`)
+
+`SOUND_STR_MIN` `SOUND_STR_MAX` controls the min-max strength of beep requests. *Seems to have no effect.* (Default `0`-`100`)
+
+`SOUND_DUR_MIN` `SOUND_DUR_MAX` controls the min-max duration of beep requests in milliseconds (Min `300`, Max `30000`) (Default `300`-`1000`)
+
+`SOUND_COOLDOWN` controls the cooldown between permitting beep requests in seconds. (Default `10`)
+
 `VIBE_STR_MIN` `VIBE_STR_MAX` controls the min-max strength of vibration requests. (Default `25`-`100`)
 
 `VIBE_DUR_MIN` `VIBE_DUR_MAX` controls the min-max duration of vibration requests in milliseconds (Min `300`, Max `30000`) (Default `300`-`1000`)
@@ -37,12 +45,30 @@ To get your Shocker ID, find the shocker you want and click the 3 dots, Edit, th
 
 `BLACKLIST` is a list of Telegram profile IDs to block from sending commands, seperated with commas (e.g: `1234567,9876543`) (Default None)
 
-`WHITELIST` controls if the blacklist should be treated as a whitelist instead. If set, the first ID is treated as the owner (if running as a bot, not a user account) to send ready notifications upon startup (`True`/`False`) (Default `False`)
+`WHITELIST` controls if the blacklist should be treated as a whitelist instead. (Default `False`)
+
+`OWNER_ID` will make the bot send successful command notices to a specified account. Will always be printed in console regardless. (Default None)
+
+Not sure what your Telegram user ID is? Message a bot like [userinfobot](https://t.me/userinfobot) to find out. (Not affiliated with this project)
+
+## Usage
+
+The bot can be run by running `shockbot.py` in Python.
+Windows users can also just use the .exe provided in the [Releases](https://github.com/DexFolf/OpenShockTelegramBot/releases).
+Press Ctrl+C in the terminal at any point to stop the bot.
+
+Upon launching the bot for the first time, you will be prompted to enter a phone number or bot token. Either enter your full phone number (including area code) or a bot token acquired from [Bot Father](https://t.me/BotFather)
+
+If everything goes well, your account name and ID will show up signifying the bot is running correctly. Commands can then be sent to that account within Telegram itself to control your shocker.
+
+If you wish to use another account, simply delete the `anon.session` file while the bot isn't running, or terminate the session through the Telegram app.
 
 ## Commands
 
 (These can be used by anyone not on the blacklist, must be sent as a PM)
 
-`/shock <strength> <duration>` sends a shock request of a given strength and duration. If any value is outside the configured range, defaults to the closest configured value
+`/sound <strength> <duration>` sends a sound request of a given strength and duration. Strength can be any number, as it doesn't make a difference in my experience.
 
-`/vibrate <strength> <duration>` sends a shock request of a given strength and duration. If any value is outside the configured range, defaults to the closest configured value
+`/shock <strength> <duration>` sends a shock request of a given strength and duration.
+
+`/vibrate <strength> <duration>` sends a vibrate request of a given strength and duration.
--- shockbot.py
+++ shockbot.py
@@ -8,32 +8,45 @@ from dotenv import load_dotenv
 logging.basicConfig(level=logging.ERROR)
 load_dotenv()
 
+# Telegram API Keys
 # Get your own values from https://my.telegram.org
 API_ID = os.environ.get('API_ID')
 API_HASH = os.environ.get('API_HASH')
 
-# Visit https://shocklink.net/#/dashboard/tokens
+# OpenShock API Key
+# Visit https://openshock.app/#/dashboard/tokens
 # Make a new token and copy the API key 
 SHOCK_API = os.environ.get('SHOCK_API')
 
-# Visit https://shocklink.net/#/dashboard/shockers/own
+# OpenShock Shock Collar ID
+# Visit https://openshock.app/#/dashboard/shockers/own
 # Click on 'Edit' and copy the ID
 SHOCK_ID = os.environ.get('SHOCK_ID')
 
-# Check if stuff is set
+# Check if required variables are set
 if None in [API_ID,API_HASH,SHOCK_API,SHOCK_ID]:
-    print('ERROR: A required environment variable hasn\'t been set! Check the README!')
-    exit()
+    raise Exception('ERROR: A required environment variable hasn\'t been set! Check the README!')
+    exit(1)
 
 # Create a Telegram client
 client = TelegramClient('anon', API_ID, API_HASH)
 
 # User preferences
+SOUND_ALLOWED = os.environ.get('SOUND_ALLOWED', 'true').lower() == 'true'
+SOUND_STR_MIN = int(os.environ.get('SOUND_STR_MIN', '0'))
+SOUND_STR_MAX = int(os.environ.get('SOUND_STR_MAX', '100'))
+SOUND_DUR_MIN = int(os.environ.get('SOUND_DUR_MIN', '300'))
+SOUND_DUR_MAX = int(os.environ.get('SOUND_DUR_MAX', '1000'))
+SOUND_COOLDOWN = int(os.environ.get('SOUND_COOLDOWN', '10'))
+
+VIBE_ALLOWED = os.environ.get('VIBE_ALLOWED', 'true').lower() == 'true'
 VIBE_STR_MIN = int(os.environ.get('VIBE_STR_MIN', '25'))
 VIBE_STR_MAX = int(os.environ.get('VIBE_STR_MAX', '100'))
 VIBE_DUR_MIN = int(os.environ.get('VIBE_DUR_MIN', '300'))
 VIBE_DUR_MAX = int(os.environ.get('VIBE_DUR_MAX', '1000'))
 VIBE_COOLDOWN = int(os.environ.get('VIBE_COOLDOWN', '10'))
+
+SHOCK_ALLOWED = os.environ.get('SHOCK_ALLOWED', 'true').lower() == 'true'
 SHOCK_STR_MIN = int(os.environ.get('SHOCK_STR_MIN', '1'))
 SHOCK_STR_MAX = int(os.environ.get('SHOCK_STR_MAX', '1'))
 SHOCK_DUR_MIN = int(os.environ.get('SHOCK_DUR_MIN', '300'))
@@ -47,115 +60,235 @@ blacklisted_ids = [int(x) for x in blacklisted_ids]
 WHITELIST = os.environ.get('WHITELIST', 'false')
 WHITELIST = WHITELIST.lower() == 'true'
 
+OWNER_ID = int(os.environ.get('OWNER_ID', 0))
+device = None
 
-# API endpoint, view more at https://api.shocklink.net/swagger/index.html (select a definition on the top right)
-url = 'https://api.shocklink.net/2/shockers/control'
-# Authenticate with OpenShock
+# OpenShock API endpoints
+# View more at https://api.openshock.app/swagger/index.html (select a definition on the top right)
+urls = {
+    'shock': 'https://api.openshock.app/2/shockers/control', # (POST) Controls shockers
+    'shock_status': 'https://api.openshock.app/1/shockers/', # (GET) Returns info about a shocker, including the device that controls it
+    'device_status' : ['https://api.openshock.app/1/devices/','/lcg'] # (GET) Returns info about a device, including the online status
+}
+
+# Message header that will authenticate with OpenShock when sending requests
 headers = {'accept': 'application/json', 'OpenShockToken': SHOCK_API, 'Content-Type': 'application/json'}
 
-# Initialise shock cooldowns (available immediately when bot started)
-next_action = datetime.now()
+# Initialise cooldowns (available immediately when bot started)
+cooldowns = {
+    'Sound': datetime.now(),
+    'Vibrate': datetime.now(),
+    'Shock': datetime.now()
+}
+
+# Friendly names for actions
+action_friendly = {
+    'Sound': 'beeped your shocker',
+    'Vibrate': 'vibrated your shocker',
+    'Shock': 'shocked you'
+}
+
+# Store permitted actions to look up later
+actions_permitted = {
+    'Sound': SOUND_ALLOWED,
+    'Vibrate': VIBE_ALLOWED,
+    'Shock': SHOCK_ALLOWED
+}
 
 # Define a general usage string
-USAGE = f'''Usage: /shock|vibrate <strength> <duration>
+USAGE = f'''Usage: /shock|vibrate|sound <strength> <duration>
 Shocks: **{SHOCK_STR_MIN}** - **{SHOCK_STR_MAX}**% strength for **{SHOCK_DUR_MIN / 1000}** - **{SHOCK_DUR_MAX / 1000}**s
-Vibrations: **{VIBE_STR_MIN}** - **{VIBE_STR_MAX}**% strength for **{VIBE_DUR_MIN / 1000}** - **{VIBE_DUR_MAX / 1000}**s'''
+Vibrations: **{VIBE_STR_MIN}** - **{VIBE_STR_MAX}**% strength for **{VIBE_DUR_MIN / 1000}** - **{VIBE_DUR_MAX / 1000}**s
+Sounds: **{SOUND_STR_MIN}** - **{SOUND_STR_MAX}**% strength for **{SOUND_DUR_MIN / 1000}** - **{SOUND_DUR_MAX / 1000}**s'''
 
-#This fuinction is used to select a value between min and max, with a default used otherwise
+# This function checks if the shocker controller is online
+async def checkOnline(SHOCK_API,SHOCK_ID):
+    global device
+    print(f'{neatTime()} | Checking if your shocker can be reached...')
+
+    # Send a request to get shocker info
+    if device is None:
+        response = requests.get(url = urls['shock_status']+SHOCK_ID, headers = headers)
+        if response.status_code == 200:
+            try:
+                device = response.json()['data']['device']
+            except:
+                print(f'{neatTime()} | Something went wrong when trying to fetch the device ID of your shocker:\n{response.content}')
+                exit(1)
+        else:
+            print(f'{neatTime()} | An error occured when trying to check your shocker ID:\n{response.content}')
+            exit(1)
+        
+    # Fetch the online status of the device that controls your shocker
+    response = requests.get(url = urls['device_status'][0]+device+urls['device_status'][1], headers = headers)
+    if response.status_code == 200:
+        print(f'{neatTime()} | Hooray! Your shocker is online and functional!')
+        return True
+    elif response.status_code == 404:
+        if response.json()['type'] == 'Device.NotOnline':
+            errormsg = 'WARNING: OpenShock reports the device your shocker is paired to is currently offline. The bot will continue to run, but you should probably get that checked out.'
+            print(f'{neatTime()} | {errormsg}')
+            await logToOwner(errormsg)
+            return False
+        else:
+            print(f'{neatTime()} | An error occured when trying to check your shock controller:\n{response.content}')
+            exit(1)
+
+
+# This function returns the current time in a neater format
+def neatTime():
+    return datetime.now().strftime('%H:%M:%S')
+
+# This function clamps supplied values to a specified range
 def clamp(min_val, max_val, default_val):
     return max(min_val, min(default_val, max_val))
 
-# This function handles the two actions
-async def command(event, cmd):
-    global next_action
+# This function handles logging messages to the owner if an ID is set
+async def logToOwner(msgtosend):
+    if owner is not None:
+        await client.send_message(owner.id, f'{msg_prefix}{msgtosend}')
+
+# This function checks if a user is allowed to use commands
+def is_allowed(event):
+    return (event.sender_id not in blacklisted_ids and not WHITELIST) or (event.sender_id in blacklisted_ids and WHITELIST)
+
+# This function handles the three actions
+async def command(event, sender, cmd):
+    global cooldowns
 
     # Get action and its parameters from the command args
     action = cmd[0].split('/')[1].capitalize()
     action_strength = cmd[1]
     action_length = int(cmd[2] * 1000)
-    action_cooldown = 0
 
     # Determine the correct length and strength depending on the type of the action
     match action:
-        case 'Shock':
-            action_strength = clamp(SHOCK_STR_MIN, SHOCK_STR_MAX, action_strength)
-            action_length = clamp(SHOCK_DUR_MIN, SHOCK_DUR_MAX, action_length)
-            action_cooldown = SHOCK_COOLDOWN
-        case 'Vibrate':
+        case 'Beep' | 'Sound':
+            action_strength = clamp(SOUND_STR_MIN, SOUND_STR_MAX, action_strength)
+            action_length = clamp(SOUND_DUR_MIN, SOUND_DUR_MAX, action_length)
+            action_cooldown = SOUND_COOLDOWN
+            action = 'Sound'
+        case 'Vibrate' | 'Vibe':
             action_strength = clamp(VIBE_STR_MIN, VIBE_STR_MAX, action_strength)
             action_length = clamp(VIBE_DUR_MIN, VIBE_DUR_MAX, action_length)
             action_cooldown = VIBE_COOLDOWN
+            action = 'Vibrate'
+        case 'Shock' | 'Zap':
+            action_strength = clamp(SHOCK_STR_MIN, SHOCK_STR_MAX, action_strength)
+            action_length = clamp(SHOCK_DUR_MIN, SHOCK_DUR_MAX, action_length)
+            action_cooldown = SHOCK_COOLDOWN
+            action = 'Shock'
 
-    sender = await client.get_entity(event.sender_id)
-    if (event.sender_id not in blacklisted_ids and not WHITELIST) or (event.sender_id in blacklisted_ids and WHITELIST):
+    # Is the action permitted in your config
+    if actions_permitted[action]:
+
         # If enough time has passed since last action
-        if datetime.now() > next_action:
-            print(f'{datetime.now()}: {action} request received from {sender.first_name} ({event.sender_id})')
+        if datetime.now() > cooldowns[action]:
+            print(f'{neatTime()} | {action} request received from {sender.first_name} ({event.sender_id})')
         
             # Disable action for <action>_COOLDOWN seconds
-            next_action = datetime.now() + timedelta(seconds = action_cooldown)
+            cooldowns[action] = datetime.now() + timedelta(seconds = action_cooldown)
             
             # Message to send to the API
             payload = {'shocks': [{'id': SHOCK_ID, 'type': action, 'intensity': action_strength,
                        'duration': action_length, 'exclusive': True}], 'customName': 'string'}
             
             # Send the request and save the response
-            response = requests.post(url = url, headers = headers, json = payload)
+            response = requests.post(url = urls['shock'], headers = headers, json = payload)
             
+            print(f'{neatTime()} | {response.content}; {action} at {action_strength}% for {action_length / 1000}s')
             # If everything went okay, reply with success
             if response.status_code == 200:
-                await event.reply(f'(OpenShock) {action} sent successfully.')
-            print(f'{datetime.now()}: {response.content}; {action} at {action_strength}% for {action_length / 1000}s')
+                await event.reply(f'{msg_prefix}{action} sent successfully.')
+                action_verb = action_friendly[action]
+                await logToOwner(f'{sender.first_name} ({event.sender_id}) just {action_verb} at {action_strength}% for {action_length / 1000}s')
+            else:
+                await event.reply(f'Something went wrong.')
+                await logToOwner(f'An error occured: {response.status_code} {response.json()['message']}')
             
         # If not enough time has passed, say how long is left
         else:
-            print(f'{datetime.now()}: {action} request from {sender.first_name} ({event.sender_id}) denied due to cooldown.')
-            await event.reply(f'(OpenShock) Next {action.lower()} available in {str((next_action - datetime.now()).seconds)} seconds.')
+            print(f'{neatTime()} | {action} request from {sender.first_name} ({event.sender_id}) denied due to cooldown.')
+            await event.reply(f'{msg_prefix}Next {action.lower()} available in {str((cooldowns[action] - datetime.now()).seconds)} second(s).')
     else:
-        print(f'{datetime.now()}: Blacklisted user {sender.first_name} ({event.sender_id}) has tried to use {action.lower()}!')
-        return
+        print(f'{neatTime()} | {action} request from {sender.first_name} ({event.sender_id}) denied due to being disabled.')
+        await event.reply(f'{msg_prefix}Sorry, that action is disabled.')
+
 
 # On new message received that starts with "/"
 @client.on(events.NewMessage(pattern='^/.+'))
 async def main(event):
     # Is it a PM?
     if event.is_private:
-        # Split args by space to a list
-        cmd = event.raw_text.lower().split(" ")
+        # Split arguments by space to a list
+        cmd = event.raw_text.lower().split(' ')
 
-        # Try to convert the len, dur args to numbers from string, if missing, set them to 0
+        # Try to convert the strength and duration arguments to numbers from string, if missing, set them to 0
         try:
-            cmd[1] = int(cmd[1])
-            cmd[2] = float(cmd[2])
+            # Use regex to strip away anything that isn't a number
+            cmd[1] = int(re.search('[0-9]*',cmd[1])[0])
+            cmd[2] = float(re.search('[0-9]*[.]?[0-9]*',cmd[2])[0])
         except IndexError:
             cmd.extend([0, 0])
-        # Reply with the usage if the args are not numbers
+        # Reply with the usage if the arguments are not numbers
         except ValueError:
-            if event.user_id not in BLACKLIST: await event.reply(f'The supplied arguments are not numbers. {USAGE}')
+            if is_allowed(event):
+                await event.reply(f'{msg_prefix}The supplied arguments are not numbers. {USAGE}')
             return
-            
-        # Handle individual commands
-        match cmd[0]:
-            case '/shock':
-                await command(event, cmd)
-            case '/vibrate':
-                await command(event, cmd)
-            case '/help':
-                await event.reply(USAGE)
-            case _:
-                await event.reply(f'Unknown command. {USAGE}')
-                print(f'{datetime.now()}: Unknown command sent.')
+        
+        sender = await client.get_entity(event.sender_id)
 
-# Message to let you know the bot is working
+        if is_allowed(event):
+            # Handle individual commands
+            # Only responds to certain commands if running as a bot account to avoid potentially conflicting with a selfbot
+            match cmd[0]:
+                case '/beep' | '/sound' | '/vibrate' | '/vibe' | '/shock' | '/zap':
+                    await command(event, sender, cmd)
+                case '/help' | '/start':
+                    if not (cmd[0] == '/start' and not me.bot):
+                        await event.reply(f'{msg_prefix}{USAGE}')
+                        print(f'{neatTime()} | {sender.first_name} ({event.sender_id}) used the {cmd[0]} command.')
+                case _ if me.bot:
+                    await event.reply(f'{msg_prefix}Unknown command. {USAGE}')
+                    print(f'{neatTime()} | Unknown command sent by {sender.first_name} ({event.sender_id})')
+        else:
+            print(f'{neatTime()} | Disallowed user {sender.first_name} ({event.sender_id}) tried to use {cmd[0]}!')
+            return
+
+# Messages to let you know the bot is working
 async def welcome_msg():
-    ready_msg = "(OpenShock) Bot Ready!"
+    global me
+    global owner
+    global msg_prefix
+
+    nolog_msg = 'Messages will only be printed to console.'
+    
+    # Fetch the current account info
     me = await client.get_me()
-    # If the blacklist is treated as a whitelist, the first whitelisted ID is treated as the owner
-    if WHITELIST and me.bot:
-        await client.send_message(blacklisted_ids[0], ready_msg)
+    # Now that we know who we are, determine if we need a prefix for every message
+    if not me.bot:
+        msg_prefix = '**(OpenShock)** '
     else:
-        await client.send_message(me.id, ready_msg)
-    print(f'{datetime.now()}: Python script running as {me.first_name} ({me.id})')
+        msg_prefix = ''
+    
+    print(f'{neatTime()} | Python script running as {me.first_name} ({me.id})')
+
+    # Let you know if logging messages will be sent, and to whom
+    if OWNER_ID != 0:
+        owner = await client.get_entity(OWNER_ID)
+        # Double check that this isn't a bot messaging a bot, Telegram doesn't like that.
+        if not me.bot or not owner.bot:
+            print(f'{neatTime()} | Messages will be sent to {owner.first_name} ({owner.id})')
+        else:
+            print(f'{neatTime()} | Telegram disallows bots messaging bots. {nolog_msg}')
+            owner = None
+    else:
+        print(f'{neatTime()} | No Owner ID specified. {nolog_msg}')
+        owner = None
+
+    # Check if your specified shocker can actually be reached then announce that the bot is ready
+    await checkOnline(SHOCK_API,SHOCK_ID)
+    await logToOwner('Bot Ready!')
 
 # Run the bot
 client.start()
